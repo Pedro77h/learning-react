0903a22c2f0529f412c9ead78f247efe9518dbf1
Pedro77h <Pedrohenriquearaujosoares25@gmail.com>
2022-12-25 20:53:26 -0300

    Learning React - Multi-stage form

--- multiform/src/components/Siderbaritem/index.tsx
+++ multiform/src/components/Siderbaritem/index.tsx
@@ -1,19 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import * as S from './styles'
+import React from "react";
+import { Link } from "react-router-dom";
+import * as S from "./styles";
+import { ReactComponent as ProfileIcon } from "../../assets/profile.svg";
+import { ReactComponent as BookIcon } from "../../assets/book.svg";
+import { ReactComponent as MailIcon } from "../../assets/mail.svg";
 
 interface Props {
-  title: string
-  description: string
-  icon: string
-  path: string
+  title: string;
+  description: string;
+  icon: string;
+  path: string;
+  active: boolean;
 }
 
-
-export const SidebarItem = ({title , description , icon , path}: Props) => {
+export const SidebarItem = ({ title, description, icon, path , active }: Props) => {
   return (
     <S.Container>
-      <Link to={path}></Link>
+      <Link to={path}>
+        <S.Info>
+          <S.Title>{title}</S.Title>
+          <S.Description>{description}</S.Description>
+        </S.Info>
+        <S.IconArea active={active}>
+          {icon === "profile" &&
+            <ProfileIcon fill="white" width={24} height={24} />
+          }
+          
+          {icon === "book" && <BookIcon fill="white" width={24} height={24} />}
+          
+          {icon === "mail" && 
+            <MailIcon fill="white" width={24} height={24} />}
+        </S.IconArea>
+        <S.Point active={active} />
+      </Link>
     </S.Container>
-  )
-}
+  );
+};
--- multiform/src/components/Theme/index.tsx
+++ multiform/src/components/Theme/index.tsx
@@ -2,31 +2,35 @@ import { ReactNode } from "react";
 import * as S from "./styles";
 import { Header } from "../Header";
 import { SidebarItem } from "../Siderbaritem";
+import { useForm } from "../../contexts/FormContext";
 
 type Props = {
   children: ReactNode;
 };
 
 export const Theme = ({ children }: Props) => {
+  const { state } = useForm();
+
   return (
     <S.Container>
       <S.Area>
         <Header />
         <S.Steps>
           <S.Sidebar>
-
             <SidebarItem
               title="Pessoal"
               description="Se indentifique"
               icon="profile"
               path="/"
+              active={state.currentStep === 1}
             />
 
             <SidebarItem
               title="Profissional"
-              description="seu nivel"
+              description="Seu nivel"
               icon="book"
               path="/step2"
+              active={state.currentStep === 2}
             />
 
             <SidebarItem
@@ -34,9 +38,10 @@ export const Theme = ({ children }: Props) => {
               description="Como te achar"
               icon="mail"
               path="/step3"
+              active={state.currentStep === 3}
             />
           </S.Sidebar>
-          
+
           <S.Page>{children}</S.Page>
         </S.Steps>
       </S.Area>
